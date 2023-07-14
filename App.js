d9c0118ef767a51cbce86371f72f90265e220147
Agustin Goni <agustingoni1710@gmail.com>
2023-07-13 23:42:02 -0300

    Segunda Entrega

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet,Text, View, TextInput, Button, StatusBar,SafeAreaView, SafeAreaViewBase} from 'react-native';
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>MDQCaps</Text>
-      <StatusBar style="auto" />
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <View style={styles.inputContainer}>
+          <TextInput style={styles.input} placeholder='add new tasks'/>
+          <button title='Create' color='#424D9E'/>
+        </View>
     </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginHorizontal: 20,
+    paddingTop: StatusBar.currentHeight
   },
+
+  inputContainer : {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  input: {
+    flex: 0.95,
+    borderWidth: 1,
+    borderRadius: 10,
+    paddingHorizontal: 10,
+    color: 'red',
+    borderColor: '#424D9E',
+    fontSize: 14,  
+  }
 });
