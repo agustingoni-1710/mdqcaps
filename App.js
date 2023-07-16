d33351eb57a73f231bb97a206ad0db5442fb73de
Agustin Goni <agustingoni1710@gmail.com>
2023-07-16 02:19:10 -0300

    Segunda Entrega

--- App.js
+++ App.js
@@ -1,42 +1,22 @@
-import { StyleSheet,Text, View, TextInput, Button, StatusBar,SafeAreaView, SafeAreaViewBase} from 'react-native';
+import {StyleSheet, Text, View, TextInput, Button, StatusBar, SafeAreaView} from 'react-native';
 
-
+import { styles } from './styles';
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.safeArea}>
+
+    // safeAreaView es un componente contenedor que solo funciona para ios que da la caracteristica de saber de donde es seguro mostrar el contenido de mi app, es decir que se vea claramente legible el contenio del contenedor 
+
+
+    <SafeAreaView style={styles.safeArea}>  
       <View style={styles.container}>
         <View style={styles.inputContainer}>
           <TextInput style={styles.input} placeholder='add new tasks'/>
-          <button title='Create' color='#424D9E'/>
+          <Button title='Create'color='#424D9E'/>
         </View>
-    </View>
-    </SafeAreaView>
+      </View>
+   </SafeAreaView> 
   );
 }
 
-const styles = StyleSheet.create({
-  safeArea: {
-    flex: 1,
-  },
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    marginHorizontal: 20,
-    paddingTop: StatusBar.currentHeight
-  },
 
-  inputContainer : {
-    flexDirection: 'row',
-    justifyContent: 'space-between'
-  },
-  input: {
-    flex: 0.95,
-    borderWidth: 1,
-    borderRadius: 10,
-    paddingHorizontal: 10,
-    color: 'red',
-    borderColor: '#424D9E',
-    fontSize: 14,  
-  }
-});
---
+++ styles.js
@@ -0,0 +1,32 @@
+import { StyleSheet, StatusBar } from "react-native";
+
+// manera de crear el import de forma rapida es posicionarse sobre el componente a importar y seleccionra ctrl+espacio
+
+export const styles = StyleSheet.create({
+    safeArea: {
+      flex: 1,
+    },
+    container: {   
+      flex: 1,// quiere decir que ocupa toda la pantalla
+      backgroundColor: '#fff',
+      marginHorizontal: 20,
+      paddingTop: StatusBar.currentHeight, // el StatusBar solo existe para android, en ios la altura es 0(es el espacio por debajo de la hora y marcador de bateria del movil)
+      
+    },
+  
+    inputContainer : {
+      flexDirection: 'row',
+      justifyContent: 'space-between'
+      
+    },
+    input: {
+      flex: 0.95,
+      borderWidth: 1,
+      borderRadius: 10,
+      paddingHorizontal: 10, // separa el texto del borde que se encuentra dentro del input
+      color: 'blue',
+      height: 40,
+      borderColor: '#424D9E',
+      fontSize: 14,  
+    }
+  });
